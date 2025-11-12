a8fb1e524a0601b7f2f6c5784d987f3a353b59e7
Kaden Cassidy <jkc.cassidy@gmail.com>
2025-11-12 15:41:42 -0800

    game state package

--- FPGA/src/game_state_pkg.sv
+++ FPGA/src/game_state_pkg.sv
@@ -1,4 +1,4 @@
-// vga_pkg.sv
+// game_state_pkg.sv
 // James Kaden Cassidy 
 // kacassidy@hmce.edu
 // 11/12/2025
@@ -8,5 +8,7 @@ package game_state_pkg;
   typedef struct {
     logic[9:0] screen[19:0];
   } game_state_t;
+
+  localparam game_state_t blank_game_state = 0;
 
 endpackage : vga_pkg
